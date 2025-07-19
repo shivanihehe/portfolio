7dcc9b28ab09cebabd3a2e428faafbd6cd38e42c
Shivanihehe <shivani1712@gmail.com>
2025-07-19 18:35:57 +0530

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 //import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/github-mark-white.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon3 from '../assets/img/x-social-media-round-icon (1).png';
 
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -53,7 +53,7 @@ export const NavBar = () => {
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/shivani-mehar-9a9860225/"><img src={navIcon1} alt="" /></a>
                 <a href="https://github.com/shivanihehe"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://x.com/shivanihehe?s=11"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
